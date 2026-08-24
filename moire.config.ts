407f499fb23a64cb711d630d0fbf46d53f2253ae
mhaitao <79950829+mhaitao@users.noreply.github.com>
2026-08-24 15:03:04 +0800

    Update Moire configuration settings

--- moire.config.ts
+++ moire.config.ts
@@ -1,11 +1,12 @@
 export const config = {
-  title: "Moire",
-  author: "Aeris",
-  theme: "classic",
+  title: "mahaitao",
+  baseUrl: "https://mahaitao.com",
+  author: "haitao",
+  theme: "bento",
   heatmap: true,
   pageSize: 20,
   order_by: "created",
-  description: "Sync your thoughts from Apple Notes by Shortcuts.",
-  keywords: "memo, thought, sync, apple notes, shortcuts, ios",
-  url: "https://moire.blog"
+  description: "",
+  keywords: "",
+  url: ""
 };
